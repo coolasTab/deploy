9e8d868327d45c81142b8f38553ec92e2a7ad42b
nikhil <n1998naik@gmail.com>
2023-01-28 19:15:17 +0530

    changed router

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use("/api/nik", (req,res) => {
     res.json({message : "Hello from nik"})
 })
 
+app.use("/api/nn", (req,res) => {
+    res.json({message : "Hello from nn"})
+})
+
 app.listen(9000, ()=> {
     console.log(`port is running on ${port}`)
 })
