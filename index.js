9685ad6b53331fb715f6b2f4ffc5d216485a96ea
nikhil <n1998naik@gmail.com>
2023-01-28 10:33:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app = express();
+const port = 9000;
+
+app.use("/", (req,res) => {
+    res.json({message : "Hello from express app"})
+})
+
+app.listen(9000, ()=> {
+    console.log(`port is running on ${port}`)
+})
